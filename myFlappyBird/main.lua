9ee9964c0b95ae2e1259c086c853e8dd7cf7fbc7
matuneville <nevillematias@gmail.com>
2024-01-30 11:36:17 -0300

    flappyBird - bird update

--- myFlappyBird/main.lua
+++ myFlappyBird/main.lua
@@ -18,6 +18,7 @@ VIRT_HEIGHT = 288
 
 BACKGROUND_DX = -30
 MOUNTAINS_BG_DX = -50
+MOUNTAINS_FG_DX = -65
 GROUND_DX = -80
 
 -- Variables
@@ -26,14 +27,18 @@ GROUND_DX = -80
 local background = love.graphics.newImage('assets/background_clouds.png')
 local ground = love.graphics.newImage('assets/ground.png')
 local mountains_bg = love.graphics.newImage('assets/mountains_bg.png')
+local mountains_fg = love.graphics.newImage('assets/mountains_fg.png')
+local bird = love.graphics.newImage('assets/bird.png')
 
 local background_width = background:getWidth()
 local ground_width = ground:getWidth()
 local mountains_bg_width = mountains_bg:getWidth()
+local mountains_fg_width = mountains_fg:getWidth()
 
 local background_scroll = 0
 local ground_scroll = 0
 local mountains_bg_scroll = 0
+local mountains_fg_scroll = 0
 
 local grounds_to_draw = 0
 local width_remaining = VIRT_WIDTH
@@ -45,6 +50,7 @@ end
 local background_loop = background_width
 local ground_loop = grounds_to_draw * ground_width
 local mountains_bg_loop = mountains_bg_width
+local mountains_fg_loop = mountains_bg_width
 
 function love.load()
 
@@ -65,6 +71,8 @@ function love.update(dt)
 
     mountains_bg_scroll = (mountains_bg_scroll + MOUNTAINS_BG_DX * dt) % mountains_bg_loop
 
+    mountains_fg_scroll = (mountains_fg_scroll + MOUNTAINS_FG_DX * dt) % mountains_fg_loop
+
     ground_scroll = (ground_scroll + GROUND_DX * dt) % ground_loop
 end
 
@@ -79,10 +87,15 @@ function love.draw()
     love.graphics.draw(mountains_bg, mountains_bg_scroll - mountains_bg_width, 0)
     love.graphics.draw(mountains_bg, mountains_bg_scroll, 0)
 
+    love.graphics.draw(mountains_fg, mountains_fg_scroll - mountains_fg_width, 0)
+    love.graphics.draw(mountains_fg, mountains_fg_scroll, 0)
+
     for i = 0, (grounds_to_draw-1)*2, 2 do
         love.graphics.draw(ground, ground_scroll - ground_width + (i-1)*ground_width, VIRT_HEIGHT-18)
         love.graphics.draw(ground, ground_scroll - ground_width + i*ground_width, VIRT_HEIGHT-18)
     end
+
+    love.graphics.draw(bird, VIRT_WIDTH/2, VIRT_HEIGHT/2)
 
     push:finish()
 end
